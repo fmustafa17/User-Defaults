835cb0dd5bf1dadad077d59cb7892be4b7803b3e
Farhana Mustafa <farhana.x.mustafa@kp.org>
2020-07-08 20:00:17 -0700

    adding UI code

---
+++ Names to Faces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Farhana Mustafa on 7/8/20.
+//  Copyright © 2020 Farhana Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
