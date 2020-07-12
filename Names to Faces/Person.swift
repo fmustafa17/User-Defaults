d1088080c1e93d4490b13addac03d5a527cb6e1c
Farhana Mustafa <farhana.x.mustafa@kp.org>
2020-07-11 20:52:04 -0700

    added collection view functionality

---
+++ Names to Faces/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Names to Faces
+//
+//  Created by Farhana Mustafa on 7/11/20.
+//  Copyright © 2020 Farhana Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
